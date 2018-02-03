73801f872ce1632c3dbb8ba0771a2052ceff2d70
Lotfi idir <lotfiidir1@gitlab.com>
2018-02-03 23:14:43 +0100

    update dependencies

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
+import MapView from 'react-native-maps';
 
 
 export default class MapScreen extends React.Component {
     render() {
         return (
-            <View style={styles.map}>
-                <Text h2>MAP VIEW</Text>
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
             </View>
         );
 
@@ -15,14 +24,13 @@ export default class MapScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        justifyContent: 'center',
     },
     map: {
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        ...StyleSheet.absoluteFillObject,
     }
 });
